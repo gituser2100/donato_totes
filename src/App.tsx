2966c1a4c099759af45d122e256a7335156a7265
gituser2100 <147813799+gituser2100@users.noreply.github.com>
2025-10-22 17:42:23 -0700

    fixing unused variable error
    ;

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import MainLayout from './Layout/MainLaout';
 import HomePage from './pages/Home';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Gallery from './pages/Gallery';
 
 const router = createBrowserRouter(
